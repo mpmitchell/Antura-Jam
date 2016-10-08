ef6cce31c10756813d45f18c2b4f1e341c428f54
Matthew Mitchell <matt.mancer@gmail.com>
2016-10-08 19:57:57 +0100

    Use FixedUpdate and increase gui font size

--- Assets/Game/Scripts/GameController.cs
+++ Assets/Game/Scripts/GameController.cs
@@ -2,24 +2,29 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
-	public float forwardSpeed = -10.0f;
-	public float horizontalSpeed = 5.0f;
-	public float limits = 20.0f;
+	[SerializeField] float forwardSpeed = -10.0f;
+	[SerializeField] float horizontalSpeed = 5.0f;
+	[SerializeField] float limits = 20.0f;
+	[SerializeField] GameObject player;
+
+  GUIStyle style;
 
 	void Start() {
 		Input.gyro.enabled = true;
+    style = new GUIStyle();
+    style.fontSize = 100;
 	}
 
-	void Update() {
-		transform.Translate(Vector3.forward * Time.deltaTime * forwardSpeed);
+	void FixedUpdate() {
+		transform.Translate(Vector3.forward * Time.fixedDeltaTime * forwardSpeed);
 
 		Vector3 currentPosition = transform.position;
 		float roll = Input.gyro.attitude.eulerAngles.y;
 
 		if ((roll < 90.0f && roll > 20.0f) || Input.GetKey("left")) {
-			currentPosition.x += Time.deltaTime * horizontalSpeed;
+			currentPosition.x += Time.fixedDeltaTime * horizontalSpeed;
 		} else if ((roll > 270.0f && roll < 340.0f) || Input.GetKey("right")) {
-			currentPosition.x -= Time.deltaTime * horizontalSpeed;
+			currentPosition.x -= Time.fixedDeltaTime * horizontalSpeed;
 		}
 
 		if (currentPosition.x > limits) {
@@ -29,5 +34,9 @@ public class GameController : MonoBehaviour {
 		}
 
 		transform.position = currentPosition;
+	}
+
+	void OnGUI() {
+		GUI.Label(new Rect(0, 0, 120, 30), Input.gyro.attitude.eulerAngles.ToString(), style);
 	}
 }
