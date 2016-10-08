1b50938b429bf9ff2e81cac60528d7a13b6fbfae
Matthew Mitchell <matt.mancer@gmail.com>
2016-10-08 15:03:04 +0100

    acc

---
+++ Assets/Game/Scripts/GameAcceleration.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class aceleration : MonoBehaviour
+{
+    public Vector3 forceVec;
+    public Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        rb.AddForce(Input.gyro.userAcceleration.x * forceVec);
+    }
+}
