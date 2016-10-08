77c74e758fb7f97ce8460aab059f2013232b8924
Matthew Mitchell <matt.mancer@gmail.com>
2016-10-08 21:42:52 +0100

    Better gyroscope controls
    Requires landscape

--- Assets/Game/Scripts/GameController.cs
+++ Assets/Game/Scripts/GameController.cs
@@ -13,20 +13,21 @@ public class GameController : MonoBehaviour {
 	void Start() {
 		Input.gyro.enabled = true;
     style = new GUIStyle();
-    style.fontSize = 100;
+    style.fontSize = 50;
 	}
 
 	void FixedUpdate() {
 		transform.Translate(Vector3.forward * Time.fixedDeltaTime * forwardSpeed);
 
 		Vector3 currentPosition = transform.position;
-		float roll = Input.gyro.attitude.eulerAngles.y;
-		float pitch = Input.gyro.attitude.eulerAngles.x;
+		float roll = GetAngleByDeviceAxis(Vector3.up);
+		float pitch = GetAngleByDeviceAxis(Vector3.right);
+		float pitchRate = Input.gyro.rotationRate.x;
 
 		if ((roll < 90.0f && roll > deadZone) || Input.GetKey("left")) {
-			currentPosition.x += Time.fixedDeltaTime * horizontalSpeed;
-		} else if ((roll > 270.0f && roll < (360.0f - deadZone)) || Input.GetKey("right")) {
 			currentPosition.x -= Time.fixedDeltaTime * horizontalSpeed;
+		} else if ((roll > 270.0f && roll < (360.0f - deadZone)) || Input.GetKey("right")) {
+			currentPosition.x += Time.fixedDeltaTime * horizontalSpeed;
 		}
 
 		if (currentPosition.x > limits) {
@@ -37,12 +38,38 @@ public class GameController : MonoBehaviour {
 
 		transform.position = currentPosition;
 
-		if ((pitch > 70.0f && pitch < 90.0f) || Input.GetKeyDown("up")) {
+		if ((Mathf.Abs(pitchRate) > 0.4 && pitch > 70.0f && pitch < 90.0f) || Input.GetKeyDown("up")) {
 			player.SendMessage("Jump");
 		}
 	}
 
 	void OnGUI() {
-		GUI.Label(new Rect(0, 0, 120, 30), Input.gyro.attitude.eulerAngles.ToString(), style);
+		GUI.Label(new Rect(0, 0, 120, 30), "Roll" + GetAngleByDeviceAxis(Vector3.up).ToString(), style);
+		GUI.Label(new Rect(0, 50, 120, 30), "Pitch" + GetAngleByDeviceAxis(Vector3.right).ToString(), style);
 	}
+
+  // http://answers.unity3d.com/answers/1208293/view.html
+  /// <summary>
+  /// Returns the rotation angle of given device axis. Use Vector3.right to obtain pitch, Vector3.up for yaw and Vector3.forward for roll.
+  /// This is for landscape mode. Up vector is the wide side of the phone and forward vector is where the back camera points to.
+  /// </summary>
+  /// <returns>A scalar value, representing the rotation amount around specified axis.</returns>
+  /// <param name="axis">Should be either Vector3.right, Vector3.up or Vector3.forward. Won't work for anything else.</param>
+  float GetAngleByDeviceAxis(Vector3 axis) {
+    Quaternion deviceRotation = Input.gyro.attitude;
+    Quaternion eliminationOfOthers = Quaternion.Inverse(
+      Quaternion.FromToRotation(axis, deviceRotation * axis)
+    );
+    Vector3 filteredEuler = (eliminationOfOthers * deviceRotation).eulerAngles;
+
+    float result = filteredEuler.z;
+    if (axis == Vector3.up) {
+      result = filteredEuler.y;
+    }
+    if (axis == Vector3.right) {
+      // incorporate different euler representations.
+      result = (filteredEuler.y > 90 && filteredEuler.y < 270) ? 180 - filteredEuler.x : filteredEuler.x;
+    }
+    return result;
+  }
 }
