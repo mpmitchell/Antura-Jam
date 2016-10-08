a72b8c22fce9d6bb9639b3ee3c6c1525f6d1d679
Matthew Mitchell <matt.mancer@gmail.com>
2016-10-08 13:39:26 +0100

    Gyrocope movement

--- Assets/Game/Scripts/GameController.cs
+++ Assets/Game/Scripts/GameController.cs
@@ -2,14 +2,32 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
-	public float speed = -10.0f;
+	public float forwardSpeed = -10.0f;
+	public float horizontalSpeed = 5.0f;
+	public float limits = 20.0f;
 
 	void Start() {
 		Input.gyro.enabled = true;
 	}
 
 	void Update() {
-		transform.Translate(Vector3.forward * Time.deltaTime * speed);
-		// Move ground left/right based on gyroscope
+		transform.Translate(Vector3.forward * Time.deltaTime * forwardSpeed);
+
+		Vector3 currentPosition = transform.position;
+		float roll = Input.gyro.attitude.eulerAngles.y;
+
+		if (roll < 90.0f) {
+			currentPosition.x += Time.deltaTime * horizontalSpeed;
+		} else if (roll > 270.0f) {
+			currentPosition.x -= Time.deltaTime * horizontalSpeed;
+		}
+
+		if (currentPosition.x > limits) {
+			currentPosition.x = limits;
+		} else if (currentPosition.x < -limits) {
+			currentPosition.x = -limits;
+		}
+
+		transform.position = currentPosition;
 	}
 }
